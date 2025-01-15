f8bed6518c024c93fec9e5fcc6f9b1362a306c17
esoty6 <kamilwitek12@o2.pl>
2025-01-15 13:55:39 +0100

    Project setup

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import pluginVitest from "@vitest/eslint-plugin";
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import oxlint from "eslint-plugin-oxlint";
+import pluginPlaywright from "eslint-plugin-playwright";
+import pluginVue from "eslint-plugin-vue";
+
+export default [
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
+  },
+
+  {
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+  },
+
+  ...pluginVue.configs["flat/essential"],
+  ...vueTsEslintConfig(),
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ["src/**/__tests__/*"],
+  },
+
+  {
+    ...pluginPlaywright.configs["flat/recommended"],
+    files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
+  },
+  oxlint.configs["flat/recommended"],
+  skipFormatting,
+
+  {
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+      },
+    ],
+  },
+];
