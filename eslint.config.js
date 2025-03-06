2bbf67aaee779bb54ab8a4a4497058b0a855f688
esoty6 <kamilwitek12@o2.pl>
2025-03-06 20:22:13 +0100

    Bumped packages, added eslint-vuetify-plugin and configured it

--- eslint.config.js
+++ eslint.config.js
@@ -4,34 +4,42 @@ import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescri
 import oxlint from "eslint-plugin-oxlint";
 import pluginPlaywright from "eslint-plugin-playwright";
 import pluginVue from "eslint-plugin-vue";
+import vuetify from "eslint-plugin-vuetify";
+import vueParser from "vue-eslint-parser";
 
 export default defineConfigWithVueTs(
-  pluginVue.configs["flat/recommended"],
   vueTsConfigs.recommended,
-
+  pluginVue.configs["flat/recommended"],
+  oxlint.configs["flat/recommended"],
+  skipFormatting,
+  {
+    name: "vuetify plugin",
+    ...vuetify.configs["flat/recommended"].rules,
+  },
   {
     name: "app/files-to-lint",
     files: ["**/*.{ts,mts,tsx,vue}"],
   },
-
   {
     name: "app/files-to-ignore",
     ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
   },
-
   {
     ...pluginVitest.configs.recommended,
     files: ["src/**/__tests__/*"],
   },
-
   {
     ...pluginPlaywright.configs["flat/recommended"],
     files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
   },
-
-  oxlint.configs["flat/recommended"],
-  skipFormatting,
-
+  {
+    files: ["*.vue", "**/*.vue"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parser: vueParser,
+    },
+  },
   {
     rules: {
       "no-unused-vars": "off",
